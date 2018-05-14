d58ad956f5c4135b07db68c49373af91102b2d66
kyloris0660 <1489821378@qq.com>
2018-05-14 18:04:06 +0800

    0w0

--- config.py
+++ config.py
@@ -5,3 +5,11 @@ PATCH_SIZE = 80
 SCALE_FACTOR = 2
 LABEL_SIZE = SCALE_FACTOR * INPUT_SIZE
 JPEG_NOICE_LEVEL = 1
+GAUSSIAN_NOISE_STD = 0.01
+BATCH_SIZE = 16
+NUM_STEPS = 1000000
+# train dataset path and etc
+TRAIN_PATH = '/Users/kyloris/Projects/image_data/data_train'
+TEST_PATH = '/Users/kyloris/Projects/image_data/data_test'
+TRAINING_SUMMARY_PATH = '/Users/kyloris/Projects/image_data/Training_summary'
+CHECKPOINT_PATH = '/Users/kyloris/Projects/image_data/checkpoint'
--- model.py
+++ model.py
@@ -34,12 +34,23 @@ def create_model(patches):
         batch_size = int(net.outputs.shape[0])
         rows = int(net.outputs.shape[1])
         cows = int(net.outputs.shape[2])
-        channels = int(patches.get_shape()[0])
+        channels = int(patches.get_shape()[3])
 
         net = tl.layers.DeConv2dLayer(net,
                                       shape=(4, 4, 3, 256),
                                       output_shape=(batch_size, rows * 2, cows * 2, channels),
                                       strides=(1, 2, 2, 1),
                                       name='vgg7_Deconv')
-        return net
+        return net.outputs
 
+
+def s_mse_loss(inference, ground_truth, name='mse_loss'):
+    with tf.name_scope(name):
+        slice_begin = (int(ground_truth.get_shape()[1]) - int(inference.get_shape()[1])) // 2
+        slice_end = int(inference.get_shape()[1]) + slice_begin
+        delta = inference - ground_truth[:, slice_begin: slice_end, slice_begin: slice_end, :]
+
+        delta *= [[[[0.11448, 0.58661, 0.29891]]]]  # weights of B, G and R
+        l2_loss = tf.pow(delta, 2)
+        mse_loss = tf.reduce_mean(tf.reduce_sum(l2_loss, axis=[1, 2, 3]))
+        return mse_loss
